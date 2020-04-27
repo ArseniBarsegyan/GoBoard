dbc09823c0330dceb0a13d1e2b43a552f8056c49
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2020-04-27 11:59:41 +0300

    Added Level label (UI and script)

---
+++ AssassinsGO/Assets/Scripts/LevelLabel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LevelLabel : MonoBehaviour
+{
+    Text m_text;
+
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+
+        if (m_text != null)
+        {
+            m_text.text = SceneManager.GetActiveScene().name;
+        }
+    }
+}
